8a95e274e5d3353f15ecfac7834aa304be4a64f9
Aseem Baranwal <aseembaranwal23@gmail.com>
2023-08-26 17:26:13 -0400

    Updated the code with latest profile changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,175 +1,180 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById('nav-menu');
-const navToggle = document.getElementById('nav-toggle');
-const navClose = document.getElementById('nav-close');
-
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', ()=>{
-        navMenu.classList.add('show-menu');
-        navMenu.classList.remove('hide-menu');
-    })
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+    navMenu.classList.remove("hide-menu");
+  });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.add('hide-menu');
-        navMenu.classList.remove('show-menu');
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.add("hide-menu");
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.add('hide-menu');
-    navMenu.classList.remove('show-menu');
+function linkAction() {
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.add("hide-menu");
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName('skills__content');
-const skillsHeader = document.querySelectorAll('.skills__header');
+const skillsContent = document.getElementsByClassName("skills__content");
+const skillsHeader = document.querySelectorAll(".skills__header");
 
-function toggleSkills(){
-    let itemClass = this.parentNode.className;
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
 
-    for(i = 0; i < skillsContent.length; i++){
-        skillsContent[i].className = 'skills__content skills__close';
-    }
-    if(itemClass === 'skills__content skills__close'){
-        this.parentNode.className = 'skills__content skills__open';
-    }
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
 }
 
-skillsHeader.forEach((el) =>{
-    el.addEventListener('click', toggleSkills);
-})
-
-
-/*==================== QUALIFICATION TABS ====================*/
-const tabs = document.querySelectorAll('[data-target]');
-const tabContents = document.querySelectorAll('[data-content]');
-
-tabs.forEach(tab =>{
-    tab.addEventListener('click', () =>{
-        const target = document.querySelector(tab.dataset.target);
-
-        tabContents.forEach(tabContent =>{
-            tabContent.classList.remove('qualification__active');
-        });
-        target.classList.add('qualification__active');
-
-        tabs.forEach(tab =>{
-            tab.classList.remove('qualification__active');
-        });
-        tab.classList.add('qualification__active');
-    })
-})
-
-
-/*==================== PROJECTS MODEL ====================*/
-const modelViews = document.querySelectorAll('.projects__model');
-const modelBtns = document.querySelectorAll('.projects__button');
-const modelCloses = document.querySelectorAll('.projects__model-close');
-
-let model = function (modelClick){
-    modelViews[modelClick].classList.add('active-model');
-}
-
-modelBtns.forEach((modelbtn, i) =>{
-    modelbtn.addEventListener('click', () =>{
-        model(i);
-    })
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
 });
 
-modelCloses.forEach((modelClose) =>{
-    modelClose.addEventListener('click', () =>{
-        modelViews.forEach((modelView) =>{
-            modelView.classList.remove('active-model');
-        });
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
     });
+    target.classList.add("qualification__active");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+    tab.classList.add("qualification__active");
+  });
+});
+
+/*==================== PROJECTS MODEL ====================*/
+const modelViews = document.querySelectorAll(".projects__model");
+const modelBtns = document.querySelectorAll(".projects__button");
+const modelCloses = document.querySelectorAll(".projects__model-close");
+
+let model = function (modelClick) {
+  modelViews[modelClick].classList.add("active-model");
+};
+
+modelBtns.forEach((modelbtn, i) => {
+  modelbtn.addEventListener("click", () => {
+    model(i);
+  });
+});
+
+modelCloses.forEach((modelClose) => {
+  modelClose.addEventListener("click", () => {
+    modelViews.forEach((modelView) => {
+      modelView.classList.remove("active-model");
+    });
+  });
 });
 
 /*==================== HANDLES SWIPER  ====================*/
 var swiper = new Swiper(".handles__container", {
-    cssMode: true,
-    loop: true,
+  cssMode: true,
+  loop: true,
 
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
-
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
-function scrollHeader(){
-    const nav = document.getElementById('header')
-    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
-/*==================== SHOW SCROLL UP ====================*/ 
-function scrollUp(){
-    const scrollUp = document.getElementById('scroll-up');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+/*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener('scroll', scrollUp)
+window.addEventListener("scroll", scrollUp);
 
-/*==================== DARK LIGHT THEME ====================*/ 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'uil-sun'
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
 /*==================================== Contact Form Changes ==================================*/
-const FormButton = document.getElementById('FormButton');
-const contactName = document.getElementById('contactName');
-const contactEmail = document.getElementById('contactEmail');
-const contactSubject = document.getElementById('contactSubject');
+const FormButton = document.getElementById("FormButton");
+const contactName = document.getElementById("contactName");
+const contactEmail = document.getElementById("contactEmail");
+const contactSubject = document.getElementById("contactSubject");
 
-FormButton.addEventListener('click', ()=>{
-    var contactForm = document.getElementById('contactForm');
-    if(contactName.value && contactEmail.value && contactSubject.value) contactForm.innerHTML = "Thank you for your message! I will reply to it once I get back. Hope you have a great day ahead :)";
+FormButton.addEventListener("click", () => {
+  var contactForm = document.getElementById("contactForm");
+  if (contactName.value && contactEmail.value && contactSubject.value)
+    contactForm.innerHTML =
+      "Thank you for your message! I will reply to it once I get back. Hope you have a great day ahead :)";
 });
